8c351405f8250b2770c7728adeee534eae2e74ef
Navid Ghahremani <ghahramani.navid@gmail.com>
2016-09-24 15:16:14 +0300

    add class attribute

--- ngletteravatar.js
+++ ngletteravatar.js
@@ -11,6 +11,7 @@ var nla = angular.module('ngLetterAvatar', []);
 nla.constant('defaultSettings', {
     alphabetcolors: ["#5A8770", "#B2B7BB", "#6FA9AB", "#F5AF29", "#0088B9", "#F18636", "#D93A37", "#A6B12E", "#5C9BBC", "#F5888D", "#9A89B5", "#407887", "#9A89B5", "#5A8770", "#D33F33", "#A2B01F", "#F0B126", "#0087BF", "#F18636", "#0087BF", "#B2B7BB", "#72ACAE", "#9C8AB4", "#5A8770", "#EEB424", "#407887"],
     textColor: '#ffffff',
+    class: '',
     defaultBorder: 'border:5px solid white',
     triangleup: 'width: 0;height: 0;border-left: 50px solid transparent;border-right: 50px solid transparent;border-bottom: 100px solid;',
     fontsize: 30, // unit in pixels
@@ -32,127 +33,132 @@ nla.constant('defaultSettings', {
  * @param {type} param2
  */
 nla.directive('ngLetterAvatar', ['defaultSettings', function (defaultSettings) {
-        return {
-            restrict: 'AE',
-            replace: true,
-            scope: {
-                alphabetcolors: '=alphabetcolors',
-                data: '@'
-            },
-            link: function (scope, element, attrs) {
+    return {
+        restrict: 'AE',
+        replace: true,
+        scope: {
+            alphabetcolors: '=alphabetcolors',
+            data: '@'
+        },
+        link: function (scope, element, attrs) {
 
-                /**
-                 * Populate the attribute values to params object
-                 * @type type
-                 */
-                var params = {
-                    charCount: attrs.charcount || defaultSettings.charCount,
-                    data: attrs.data,
-                    textColor: defaultSettings.textColor,
-                    height: attrs.height || defaultSettings.height,
-                    width: attrs.width || defaultSettings.width,
-                    fontsize: attrs.fontsize || defaultSettings.fontsize,
-                    fontWeight: attrs.fontweight || defaultSettings.fontWeight,
-                    fontFamily: attrs.fontfamily || defaultSettings.fontFamily,
-                    avatarBorderStyle: attrs.avatarcustomborder,
-                    avatardefaultBorder: attrs.avatarborder,
-                    defaultBorder: defaultSettings.defaultBorder,
-                    shape: attrs.shape,
-                    alphabetcolors: scope.alphabetcolors || defaultSettings.alphabetcolors,
-                    avatarCustomBGColor: attrs.avatarcustombgcolor || defaultSettings.custombgcolor,
-                    dynamic: attrs.dynamic || defaultSettings.dynamic,
-                    rotatedeg: attrs.rotatedeg || defaultSettings.rotatedeg
-                };
+            /**
+             * Populate the attribute values to params object
+             * @type type
+             */
+            var params = {
+                charCount: attrs.charcount || defaultSettings.charCount,
+                data: attrs.data,
+                textColor: defaultSettings.textColor,
+                class: attrs.class || defaultSettings.class,
+                height: attrs.height || defaultSettings.height,
+                width: attrs.width || defaultSettings.width,
+                fontsize: attrs.fontsize || defaultSettings.fontsize,
+                fontWeight: attrs.fontweight || defaultSettings.fontWeight,
+                fontFamily: attrs.fontfamily || defaultSettings.fontFamily,
+                avatarBorderStyle: attrs.avatarcustomborder,
+                avatardefaultBorder: attrs.avatarborder,
+                defaultBorder: defaultSettings.defaultBorder,
+                shape: attrs.shape,
+                alphabetcolors: scope.alphabetcolors || defaultSettings.alphabetcolors,
+                avatarCustomBGColor: attrs.avatarcustombgcolor || defaultSettings.custombgcolor,
+                dynamic: attrs.dynamic || defaultSettings.dynamic,
+                rotatedeg: attrs.rotatedeg || defaultSettings.rotatedeg
+            };
 
-                /**
-                 * to generate the avatar dynamically on data change, enable the below function to watch the event
-                 */
-                if (params.dynamic === 'true') {
-                    scope.$watch('data', function () {
-                        _generateLetterAvatar();
-                    });
-                } else {
+            /**
+             * to generate the avatar dynamically on data change, enable the below function to watch the event
+             */
+            if (params.dynamic === 'true') {
+                scope.$watch('data', function () {
                     _generateLetterAvatar();
-                }
+                });
+            } else {
+                _generateLetterAvatar();
+            }
 
-                function _generateLetterAvatar() {
-                    var c = '';
-                    if (params.charCount == 2) {
-                        var _data = getFirstAndLastName(scope.data.toUpperCase());
-                        if (_data) {
-                            c = _data;
-                        } else {
-                            c = scope.data.substr(0, params.charCount).toUpperCase();
-                        }
+            function _generateLetterAvatar() {
+                var c = '';
+                if (params.charCount == 2) {
+                    var _data = getFirstAndLastName(scope.data.toUpperCase());
+                    if (_data) {
+                        c = _data;
                     } else {
                         c = scope.data.substr(0, params.charCount).toUpperCase();
                     }
-                    var cobj = getCharacterObject(c, params.textColor, params.fontFamily, params.fontWeight, params.fontsize);
-                    var colorIndex = '';
-                    var color = '';
+                } else {
+                    c = scope.data.substr(0, params.charCount).toUpperCase();
+                }
+                var cobj = getCharacterObject(c, params.textColor, params.fontFamily, params.fontWeight, params.fontsize);
+                var colorIndex = '';
+                var color = '';
 
-                    /**
-                     * Populate the colors according to attributes
-                     */
-                    if (c.charCodeAt(0) < 65) {
-                        color = getRandomColors();
-                    } else {
-                        colorIndex = Math.floor((c.charCodeAt(0) - 65) % params.alphabetcolors.length);
-                        color = params.alphabetcolors[colorIndex];
-                    }
+                /**
+                 * Populate the colors according to attributes
+                 */
+                if (c.charCodeAt(0) < 65) {
+                    color = getRandomColors();
+                } else {
+                    colorIndex = Math.floor((c.charCodeAt(0) - 65) % params.alphabetcolors.length);
+                    color = params.alphabetcolors[colorIndex];
+                }
 
-                    if (params.avatarCustomBGColor) {
-                        color = params.avatarCustomBGColor;
-                    }
+                if (params.avatarCustomBGColor) {
+                    color = params.avatarCustomBGColor;
+                }
 
-                    var svg = getImgTag(params.width, params.height, color);
-                    svg.append(cobj);
-                    var lvcomponent = angular.element('<div>').append(svg.clone()).html();
-                    var svgHtml = window.btoa(unescape(encodeURIComponent(lvcomponent)));
-                    var component;
-                    var base = defaultSettings.base;
-                    var _style = '';
-                    if (params.avatarBorderStyle) {
-                        _style = params.avatarBorderStyle;
-                    } else if (params.avatardefaultBorder) {
-                        _style = params.defaultBorder;
-                    }
+                var svg = getImgTag(params.width, params.height, color);
+                svg.append(cobj);
+                var lvcomponent = angular.element('<div>').append(svg.clone()).html();
+                var svgHtml = window.btoa(unescape(encodeURIComponent(lvcomponent)));
+                var component;
+                var base = defaultSettings.base;
+                var _style = '';
+                if (params.avatarBorderStyle) {
+                    _style = params.avatarBorderStyle;
+                } else if (params.avatardefaultBorder) {
+                    _style = params.defaultBorder;
+                }
 
-                    if (params.rotatedeg != '0') {
-                        _style = '-ms-transform: rotate(' + params.rotatedeg + 'deg); -webkit-transform: rotate(' + params.rotatedeg + 'deg); transform: rotate(' + params.rotatedeg + 'deg)';
-                    }
+                if (params.rotatedeg != '0') {
+                    _style = '-ms-transform: rotate(' + params.rotatedeg + 'deg); -webkit-transform: rotate(' + params.rotatedeg + 'deg); transform: rotate(' + params.rotatedeg + 'deg)';
+                }
 
-                    if (params.shape) {
-                        if (params.shape === 'round') {
-                            var round_style = defaultSettings.radius + _style;
-                            if (scope.data.indexOf('http') > -1 || scope.data.indexOf('data:image') > -1) {
-                                var img_size = 'width:' + params.width + 'px;height:' + params.height + 'px;';
-                                component = "<img src=" + scope.data + " style='" + img_size + round_style + "'  />";
-                            } else {
-                                component = "<img src=" + base + svgHtml + " style='" + round_style + "' title='" + scope.data + "' />";
-                            }
-                        }
-                    } else {
+                if (params.shape) {
+                    if (params.shape === 'round') {
+                        var round_style = defaultSettings.radius + _style;
                         if (scope.data.indexOf('http') > -1 || scope.data.indexOf('data:image') > -1) {
                             var img_size = 'width:' + params.width + 'px;height:' + params.height + 'px;';
-                            component = "<img src=" + scope.data + " style='" + img_size + _style + "'  />";
+                            component = "<img src=" + scope.data + " style='" + img_size + round_style + "'  />";
                         } else {
-                            component = "<img src=" + base + svgHtml + " style='" + _style + "' title='" + scope.data + "' />";
+                            component = "<img src=" + base + svgHtml + " style='" + round_style + "' title='" + scope.data + "' />";
                         }
                     }
-
-                    if (params.dynamic === 'true') {
-                        element.empty();
-                        element.append(component);
+                } else {
+                    if (scope.data.indexOf('http') > -1 || scope.data.indexOf('data:image') > -1) {
+                        var img_size = 'width:' + params.width + 'px;height:' + params.height + 'px;';
+                        component = "<img src=" + scope.data + " style='" + img_size + _style + "'  />";
                     } else {
-                        element.replaceWith(component);
+                        component = "<img src=" + base + svgHtml + " style='" + _style + "' title='" + scope.data + "' />";
                     }
                 }
+
+                var img;
+
+                if (params.dynamic === 'true') {
+                    element.empty();
+                    img = element.append(component);
+                } else {
+                    img = element.replaceWith(component);
+                }
+
+                img.addClass(params.class);
             }
-        };
-    }]);
+        }
+    };
+}]);
 /**
- * Get the random colors 
+ * Get the random colors
  * @returns {String}
  */
 function getRandomColors() {
@@ -192,17 +198,17 @@ function getFirstAndLastName(data) {
 function getImgTag(width, height, color) {
 
     var svgTag = angular.element('<svg></svg>')
-            .attr({
-                'xmlns': 'http://www.w3.org/2000/svg',
-                'pointer-events': 'none',
-                'width': width,
-                'height': height
-            })
-            .css({
-                'background-color': color,
-                'width': width + 'px',
-                'height': height + 'px'
-            });
+        .attr({
+            'xmlns': 'http://www.w3.org/2000/svg',
+            'pointer-events': 'none',
+            'width': width,
+            'height': height
+        })
+        .css({
+            'background-color': color,
+            'width': width + 'px',
+            'height': height + 'px'
+        });
 
     return svgTag;
 }
@@ -218,20 +224,20 @@ function getImgTag(width, height, color) {
  */
 function getCharacterObject(character, textColor, fontFamily, fontWeight, fontsize) {
     var textTag = angular.element('<text text-anchor="middle"></text>')
-            .attr({
-                'y': '50%',
-                'x': '50%',
-                'dy': '0.35em',
-                //'stroke': '#000000',
-                'pointer-events': 'auto',
-                'fill': textColor,
-                'font-family': fontFamily
-            })
-            .html(character)
-            .css({
-                'font-weight': fontWeight,
-                'font-size': fontsize + 'px',
-            });
+        .attr({
+            'y': '50%',
+            'x': '50%',
+            'dy': '0.35em',
+            //'stroke': '#000000',
+            'pointer-events': 'auto',
+            'fill': textColor,
+            'font-family': fontFamily
+        })
+        .html(character)
+        .css({
+            'font-weight': fontWeight,
+            'font-size': fontsize + 'px',
+        });
 
     return textTag;
 }
